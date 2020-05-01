c94f2f429d7d051693f665eb7d3e27a96b2af121
jouluihminen <>
2020-05-01 17:38:34 +0300

    node-tehtävä lisätty

---
+++ node.js
@@ -0,0 +1,2 @@
+// 45. Toteuta node.js:llä REST api, jonka avulla voidaan hakea KAIKKI asiakkaat tietokannasta (Asiakas-taulusta).
+// Ei ollut yhtään node-tehtävää tehtynä, kai tämäkin tehtävänannon täyttää.
